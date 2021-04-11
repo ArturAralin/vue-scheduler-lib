f9ed6aa57157b324c9c4c4d226a0ba1d5638f3cd
Artur Aralin <artur.aralin97@gmail.com>
2021-04-11 12:01:54 +0300

    update tesing stand

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Scheduler
       :events="events"
+      @eventDragged="eventDragged"
       month="2021-04"
     />
   </div>
@@ -9,7 +10,9 @@
 
 <script lang="ts">
 import { parseISO } from 'date-fns';
-import { Component, Vue } from 'vue-property-decorator';
+import addDays from 'date-fns/addDays';
+import getDate from 'date-fns/getDate/index';
+import Vue from 'vue';
 import Scheduler from './vue-scheduler/Scheduler.vue';
 
 // // eslint-disable-next-line
@@ -17,45 +20,62 @@ import Scheduler from './vue-scheduler/Scheduler.vue';
 // import Scheduler from 'vue-scheduler-lib';
 // import 'vue-scheduler-lib/dist/Scheduler.vue.css';
 
-@Component({
+export default Vue.extend({
+  name: 'App',
   components: {
     Scheduler,
   },
-})
-export default class App extends Vue {
-  events = [
-    {
-      id: '12ssss3',
-      from: parseISO('2021-04-16'),
-      to: parseISO('2021-04-16'),
-      summary: 'Поездка в Тихорецк',
+  data: () => ({
+    events: [
+      {
+        id: '12ssss3',
+        from: parseISO('2021-04-16'),
+        to: parseISO('2021-04-16'),
+        summary: 'Поездка в Тихорецк',
+      },
+      {
+        id: '123',
+        from: parseISO('2021-04-09'),
+        to: parseISO('2021-04-10'),
+        summary: 'first',
+      },
+      {
+        id: '321',
+        from: parseISO('2021-04-09'),
+        to: parseISO('2021-04-11'),
+        summary: 'second',
+      },
+      {
+        id: '1232423',
+        from: parseISO('2021-04-09'),
+        to: parseISO('2021-04-13'),
+        summary: 'really big',
+      },
+      {
+        id: '222',
+        from: parseISO('2021-04-10'),
+        to: parseISO('2021-04-10'),
+        summary: 'between',
+      },
+    ],
+  }),
+  methods: {
+    eventDragged(dragEvent: any) {
+      this.events = this.events.map((e) => {
+        if (e.id === dragEvent.eventId) {
+          const diff = getDate(dragEvent.date) - getDate(e.from);
+          return {
+            ...e,
+            from: addDays(e.from, diff),
+            to: addDays(e.to, diff),
+          };
+        }
+
+        return e;
+      });
     },
-    {
-      id: '123',
-      from: parseISO('2021-04-09'),
-      to: parseISO('2021-04-10'),
-      summary: 'first',
-    },
-    {
-      id: '321',
-      from: parseISO('2021-04-09'),
-      to: parseISO('2021-04-11'),
-      summary: 'second',
-    },
-    {
-      id: '1232423',
-      from: parseISO('2021-04-09'),
-      to: parseISO('2021-04-13'),
-      summary: 'really big',
-    },
-    {
-      id: '222',
-      from: parseISO('2021-04-10'),
-      to: parseISO('2021-04-10'),
-      summary: 'between',
-    },
-  ];
-}
+  },
+});
 </script>
 
 <style lang="scss">
